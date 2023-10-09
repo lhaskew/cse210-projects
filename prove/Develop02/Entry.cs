2a72bc73388895c7e8dab36200963c4a2bacdbe8
Lucy <has21013@byui.edu>
2023-10-09 08:26:27 -0600

    Pull from work computer

---
+++ prove/Develop02/Entry.cs
@@ -0,0 +1,19 @@
+
+public class Entry
+{
+    public string Prompt { get; set; }
+    public string Response { get; set; }
+    public DateTime Date { get; set; }
+
+    public Entry(string prompt, string response)
+    {
+        Prompt = prompt;
+        Response = response;
+        Date = DateTime.Now;
+    }
+
+    public override string ToString()
+    {
+        return $"Date: {Date}\nPrompt: {Prompt}\nResponse: {Response}\n";
+    }
+}
